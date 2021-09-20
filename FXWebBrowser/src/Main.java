e5e074c1e348e178f9c6fc5c825751dfb259202a
Thomas <Thomas@DESKTOP-Q35PG5V>
2021-09-21 00:26:57 +1200

    Change title and url bar on successful load

--- FXWebBrowser/src/Main.java
+++ FXWebBrowser/src/Main.java
@@ -2,7 +2,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -12,25 +11,25 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.web.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	private WebView webView = new WebView();
-	private WebEngine webEngine = webView.getEngine();
+	private Stage primaryStage;
+	private WebView browser = new WebView();
+	private WebEngine engine = browser.getEngine();
+	private WebHistory history; // To be used later in going forwards and backwards + browsing history
 
 	private TextField urlField;
 
-	// Regular expression pattern to match on valid url
+	// Regular expression pattern to match on valid URL with top level domain
 	private Pattern domainPattern = Pattern.compile(
 			"[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{2,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)",
 			Pattern.CASE_INSENSITIVE);
 	private Matcher domainMatcher;
 
+	// Event handler for loading a URL from user input
 	private EventHandler<ActionEvent> loadURLHandler = new EventHandler<ActionEvent>() {
-
-		@Override
 		public void handle(ActionEvent arg0) {
 			// Check if user input matches a valid typed URL
 			String urlInput = urlField.getText();
@@ -44,38 +43,51 @@ public class Main extends Application {
 			}
 		}
 	};
+	
+	// Code to do something on page load failing or succeeding
+			/*
+			 * NEED TO ASK WHY THIS CODE IS RUNNING MULTIPLE TIMES IF YOU TRY A FAIL MORE
+			 * THAN ONCE
+			 */
+	private ChangeListener<State> loadListener = new ChangeListener<State>() {
+		public void changed(ObservableValue<? extends State> ov, State oldState, State newState) {
+			if (newState == State.FAILED) {
+				// TO DO: Make some kind of page for failing to load message
+				System.out.println("Failed.");
+			}
+			if (newState == State.SUCCEEDED) {
+				// Updates text field and window title on page load
+				urlField.setText(engine.getLocation());
+				primaryStage.setTitle(engine.getTitle());
+			}
+		}
+	};
 
 	public Main() {
 		// TODO Auto-generated constructor stub
 	}
 
+	// Makes the website load!
 	public void loadURL(String url) {
 		String urlInput = toURL(url);
 		// Turns a valid typed URL into one that can be loaded by adding https:// to it
 		if (urlInput == null) {
 			urlInput = toURL("https://" + url);
-		}
-		// Code to do something on page load failing
-		/*
-		 * NEED TO ASK WHY THIS CODE IS RUNNING MULTIPLE TIMES IF YOU TRY A FAIL MORE THAN ONCE
-		 */
-		webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
-			public void changed(ObservableValue ov, State oldState, State newState) {
-				if (newState == State.FAILED) {
-					System.out.println("Failed.");
-				}
-			}
-		});
-		webEngine.load(urlInput);
+		}		
+		engine.load(urlInput);
+		primaryStage.setTitle(engine.getTitle());
+
 	}
 
-	// To use later with any url not containing a top level domain
+	// To use with any URL that does not contain a top level domain. Will look it up
+	// in a search engine instead of trying to load the URL.
 	public void searchEngineLookup(String string) {
-		webView.getEngine().load(String.format("https://www.google.com/search?q=%s", string));
+		engine.load(String.format("https://www.google.com/search?q=%s", string));
 	}
 
 	// Checks if string input is a valid URL. Returns the string if valid, otherwise
-	// returns null.
+	// returns null. This mostly is checking for "https://" at the start since the
+	// regular expression should take anything that's completely invalid.
 	private String toURL(String string) {
 		try {
 			return new URL(string).toExternalForm();
@@ -87,29 +99,37 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		Button launchBtn = new Button("Launch");
-		urlField = new TextField();
+		this.primaryStage = primaryStage;
 
-		HBox hBox = new HBox();
-		hBox.getChildren().addAll(launchBtn, urlField);
+		Button back = new Button("back");
+		Button forward = new Button("forward");
+		Button reload = new Button("reload");
+		Button home = new Button("home");
+		urlField = new TextField();
+		Button launch = new Button("launch");
+
+		HBox navigationBar = new HBox();
+		navigationBar.getChildren().addAll(back, forward, reload, home, urlField, launch);
 
 		// Set handlers for loading URL from text field
-		launchBtn.setOnAction(loadURLHandler);
+		launch.setOnAction(loadURLHandler);
 		urlField.setOnAction(loadURLHandler);
 
 		VBox root = new VBox();
-		root.getChildren().addAll(hBox, webView);
+		root.getChildren().addAll(navigationBar, browser);
 
-		VBox.setVgrow(webView, Priority.ALWAYS);
+		VBox.setVgrow(browser, Priority.ALWAYS);
 		HBox.setHgrow(urlField, Priority.ALWAYS);
 		
+		// Add listener to do things on page load failing or succeeding
+		engine.getLoadWorker().stateProperty().addListener(loadListener);
+
 		// Load home page (make this a proper method later)
-		webEngine.load("http://www.google.com");
+		engine.load("http://www.google.com");
 
 		primaryStage.setScene(new Scene(root));
 		primaryStage.sizeToScene();
 		primaryStage.show();
-
 	}
 
 	public static void main(String[] args) {
